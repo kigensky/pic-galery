0ba0ed045a36a886728bfda281c2d379f5e47c7e
victor kigen <vickigen@gmail.com>
2021-05-16 13:12:16 +0300

    added path

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,7 +9,7 @@ app_name = 'pictures'
 urlpatterns = [
     path('', views.index, name='index'),
     path('search', views.search_results, name='search'),
-    path('location/<str:location>', views.image_location, name='location'),
+    url(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
