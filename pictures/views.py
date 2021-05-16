1a39263aa528f663c0706eaead259d06d35a3a3d
victor kigen <vickigen@gmail.com>
2021-05-16 13:11:12 +0300

    added image functionality

--- pictures/views.py
+++ pictures/views.py
@@ -9,13 +9,13 @@ def index(request):
     images = Image.objects.all()
     locations = Location.get_locations()
     print(locations)
-    return render(request, 'pictures/index.html', {'images': images[::-1], 'locations': locations})
+    return render(request, 'index.html', {'images': images[::-1], 'locations': locations})
 
 
 def image_location(request, location):
     images = Image.filter_by_location(location)
     print(images)
-    return render(request, 'pictures/location.html', {'location_images': images})
+    return render(request, 'location.html', {'location_images': images})
 
 
 def search_results(request):
@@ -24,7 +24,7 @@ def search_results(request):
         searched_images = Image.search_by_category(category)
         message = f"{category}"
         print(searched_images)
-        return render(request, 'pictures/search_results.html', {"message": message, "images": searched_images})
+        return render(request, 'search_results.html', {"message": message, "images": searched_images})
     else:
         message = "You haven't searched for any image category"
-        return render(request, 'pictures/search_results.html', {"message": message})
+        return render(request, 'search_results.html', {"message": message})
